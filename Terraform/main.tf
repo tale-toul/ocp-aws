9253e1f764b0923066224f5890b884093f585d2f
Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>
2020-03-10 09:37:23 +0100

    Terraform vars moved to a separate file. Default route to nat gateways use the correct nat_gateway_id.  ssh key name uses a random suffix string to create a unique name.  DNS private and pubic zones use the same domain name.
    All terraform variable definitions have been moved to a new file.
    
    The route definition for the default route via the nat gateway uses nat_gatway_id instead of gateway_id, both work but the latter causes terraform to always create a difference in the route even when nothing has changed
    
    The ssh key name does not use a variable anymore to get its value, instead it uses a fixed prefix "ssh-key-" followed by a random string of 5 character to ensure uniqueness.
    
    Both the public and private DNS zones use the same domain name now, the don't collide though.
    
    Signed-off-by: Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,169 +12,6 @@ provider "random" {
   version = "~> 2.2"
 }
 
-#VARIABLES
-variable "region_name" {
-  description = "AWS Region where the cluster is deployed"
-  type = string
-  default = "eu-west-1"
-}
-
-variable "cluster_name" {
-  description = "Cluster name, used to define Clusterid tag and as part of other component names"
-  type = string
-  default = "ocp"
-}
-
-variable "vpc_name" {
-  description = "Name assigned to the VPC"
-  type = string
-  default = "volatil"
-}
-
-variable "dns_domain_ID" {
-  description = "Zone ID for the route 53 DNS domain that will be used for this cluster"
-  type = string
-  default = "Z1UPG9G4YY4YK6"
-}
-
-#Not all instances types are available in all regions
-#Depending on the instance type used, the device for the addtional disks use the name xvd<letter> (xvda) or nvme<number>n1 (nvme1n1)
-variable "master-instance-type" {
-  description = "Type of instance used for master nodes, define the hardware characteristics like memory, cpu, network capabilities"
-  type = string
-  default = "t3.xlarge"
-}
-
-variable "nodes-instance-type" {
-  description = "Type of instance used for infra and worker nodes, define the hardware characteristics like memory, cpu, network capabilities"
-  type = string
-  default = "t3.xlarge"
-}
-
-variable "rhel7-ami" {
-  description = "AMI on which the EC2 instances are based on, depends on the region"
-  type = map
-  default = {
-    eu-central-1   = "ami-0b5edb134b768706c"
-    eu-west-1      = "ami-0404b890c57861c2d"
-    eu-west-2      = "ami-0fb2dd0b481d4dc1a"
-    eu-west-3      = "ami-0dc7b4dac85c15019"
-    eu-north-1     = "ami-030b10a31b2b6df19"
-    us-east-1      = "ami-0e9678b77e3f7cc96"
-    us-east-2      = "ami-0170fc126935d44c3"
-    us-west-1      = "ami-0d821453063a3c9b1"
-    us-west-2      = "ami-0c2dfd42fa1fbb52c"
-    sa-east-1      = "ami-09de00221562b0155"
-    ap-south-1     = "ami-0ec8900bf6d32e0a8"
-    ap-northeast-1 = "ami-0b355f24363d9f357"
-    ap-northeast-2 = "ami-0bd7fd9221135c533"
-    ap-southeast-1 = "ami-097e78d10c4722996"
-    ap-southeast-2 = "ami-0f7bc77e719f87581"
-    ca-central-1   = "ami-056db5ae05fa26d11"
-  }
-}
-
-variable "ssh-keyfile" {
-  description = "Name of the file with public part of the SSH key to transfer to the EC2 instances"
-  type = string
-  default = "ocp-ssh.pub"
-}
-
-variable "ssh-keyname" {
-  description = "Name of the key that will be imported into AWS"
-  type = string
-  default = "ssh-key"
-}
-
-variable "master_count" {
-  description = "Number of master nodes in the OCP cluser, can only be 1 or 3"
-  type = number
-  default = 3
-}
-
-variable "infra_count" {
-  description = "Number of node instance to be used as infras in the OCP cluster"
-  type = number
-  default = 3
-}
-
-variable "worker_count" {
-  description = "Number of node instance to be used as workers in the OCP cluster"
-  type = number
-  default = 3
-}
-
-variable "user-data-masters" {
-  description = "User data for master instances"
-  type = string
-  default = <<-EOF
-       #cloud-config
-       cloud_config_modules:
-       - disk_setup
-       - mounts
-       - cc_write_files
-
-       write_files:
-       - content: |
-           STORAGE_DRIVER=overlay2
-           DEVS=/dev/nvme1n1
-           VG=dockervg
-           CONTAINER_ROOT_LV_NAME=dockerlv
-           CONTAINER_ROOT_LV_MOUNT_PATH=/var/lib/docker
-           CONTAINER_ROOT_LV_SIZE=100%FREE
-         path: "/etc/sysconfig/docker-storage-setup"
-         permissions: "0644"
-         owner: "root"
-
-       fs_setup:
-       - label: ocp_emptydir
-         filesystem: xfs
-         device: /dev/nvme2n1
-         partition: auto
-       - label: etcd
-         filesystem: xfs
-         device: /dev/nvme3n1
-         partition: auto
-
-       mounts:
-       - [ "LABEL=ocp_emptydir", "/var/lib/origin/openshift.local.volumes", xfs, "defaults,gquota" ]
-       - [ "LABEL=etcd", "/var/lib/etcd", xfs, "defaults,gquota" ]
-  EOF
-}
-
-variable "user-data-nodes" {
-  description = "User data for worker and infra nodes instances"
-  type = string
-  default = <<-EOF
-       #cloud-config
-       cloud_config_modules:
-       - disk_setup
-       - mounts
-       - cc_write_files
-
-       write_files:
-       - content: |
-           STORAGE_DRIVER=overlay2
-           DEVS=/dev/xvdb
-           VG=dockervg
-           CONTAINER_ROOT_LV_NAME=dockerlv
-           CONTAINER_ROOT_LV_MOUNT_PATH=/var/lib/docker
-           CONTAINER_ROOT_LV_SIZE=100%FREE
-         path: "/etc/sysconfig/docker-storage-setup"
-         permissions: "0644"
-         owner: "root"
-
-       fs_setup:
-       - label: ocp_emptydir
-         filesystem: xfs
-         device: /dev/xvdc
-         partition: auto
-
-       mounts:
-       - [ "LABEL=ocp_emptydir", "/var/lib/origin/openshift.local.volumes", xfs, "defaults,gquota" ]
-  EOF
-}
-
 #VPC
 resource "aws_vpc" "vpc" {
     cidr_block = "172.20.0.0/16"
@@ -337,7 +174,7 @@ resource "aws_route_table" "rtable_priv" {
 
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.natgw[count.index].id
+        nat_gateway_id = aws_nat_gateway.natgw[count.index].id
     }
     tags = {
         Name = "rtable_priv.${count.index}"
@@ -681,7 +518,7 @@ resource "aws_elb" "elb-infra-public" {
 #EC2s
 #SSH key
 resource "aws_key_pair" "ssh-key" {
-  key_name = var.ssh-keyname
+  key_name = "ssh-key-${random_string.sufix_name.result}"
   public_key = file("${path.module}/${var.ssh-keyfile}")
 }
 
@@ -828,7 +665,7 @@ data "aws_route53_zone" "domain" {
 
 #External hosted zone, this is a public zone because it is not associated with a VPC
 resource "aws_route53_zone" "external" {
-  name = "${var.cluster_name}ext.${data.aws_route53_zone.domain.name}"
+  name = "${var.cluster_name}.${data.aws_route53_zone.domain.name}"
 
   tags = {
     Name = "external"
@@ -840,7 +677,7 @@ resource "aws_route53_zone" "external" {
 
 resource "aws_route53_record" "external-ns" {
   zone_id = data.aws_route53_zone.domain.zone_id
-  name    = "${var.cluster_name}ext.${data.aws_route53_zone.domain.name}"
+  name    = "${var.cluster_name}.${data.aws_route53_zone.domain.name}"
   type    = "NS"
   ttl     = "30"
 
@@ -854,7 +691,7 @@ resource "aws_route53_record" "external-ns" {
 
 #Internal hosted zone, this is a private zone because it is associated with a VPC
 resource "aws_route53_zone" "internal" {
-  name = "${var.cluster_name}int.${data.aws_route53_zone.domain.name}"
+  name = "${var.cluster_name}.${data.aws_route53_zone.domain.name}"
 
   vpc {
     vpc_id = aws_vpc.vpc.id
